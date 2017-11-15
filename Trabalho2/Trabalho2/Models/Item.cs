efd223d36f3d974b7623b17d344f982363521511
Werlon Guilherme <werlong@gmail.com>
2017-11-15 17:45:25 -0200

    Adicionar arquivos de projeto.

---
+++ Trabalho2/Trabalho2/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Trabalho2
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
